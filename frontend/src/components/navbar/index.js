997589200ab3d36bf3259068849b62d46aee3b97
Manik <manikproch9858@gmail.com>
2024-07-26 21:20:17 +0530

    Profile page updated

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,26 +1,39 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { appLogout } from "../../store/slices/authSlice";
-import navicon from "../../public/assests/cloud-computing.png";
+import navicon from "../../public/assests/cloud-computing.png"; 
+import profileIcon from "../../public/assests/man.png"; 
 import "./styles.css";
 
 const Navbar = () => {
-  const dispatch = useDispatch();
+  const { isAuthorized } = useSelector((state) => state.auth);
 
-  const handleLogout = () => {
-    dispatch(appLogout());
-  };
+
 
   return (
     <div className="navbar-container">
       <div className="navbar-left-item">
-        <img src={navicon} alt="navicon" className="navicon" />
-        <h2>Cloud Nesttt</h2>
+        {isAuthorized ? (
+          <Link to="/">
+            <img src={navicon} alt="navicon" className="navicon" />
+          </Link>
+        ) : (
+          <img src={navicon} alt="navicon" className="navicon" />
+        )}
+        <h2>CloudNest</h2>
+      </div>
+      <div className="navbar-center-item">
+        <Link to="/features" className="nav-link">Features</Link>
+        <Link to="/about" className="nav-link">About</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
       </div>
       <div className="navbar-right-item">
-        <Link to="/profile" className="nav-link">Profile</Link>
-        <button onClick={handleLogout}>Logout</button>
+        {isAuthorized && (
+          <Link to="/profile" className="profile-link">
+            <img src={profileIcon} alt="profile" className="profile-icon" />
+          </Link>
+        )}
       </div>
     </div>
   );
--- frontend/src/pages/profilePage.js
+++ frontend/src/pages/profilePage.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { appLogout } from "../store/slices/authSlice";
 import Navbar from "../components/navbar/index";
-import "./profilePage.css";
 
 const ProfilePage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isAuthorized } = useSelector((e) => e.auth);
+
+  const handleLogout = () => {
+    dispatch(appLogout());
+  };
+
+  useEffect(() => {
+    if (!isAuthorized) {
+      navigate("/login");
+    }
+  }, [isAuthorized, navigate]);
+
   return (
-    <div className="profile-page-container">
+    <>
       <Navbar />
-      <div className="profile-content">
-        <h1>Profile Page</h1>
-        <p>Welcome to your profile!</p>
-        {/* Add more profile information here */}
+      <div className="profile-page-container">
+        <div className="profile-content">
+          <h1>Profile Page</h1>
+          <p>Welcome to your profile!</p>
+          <button onClick={handleLogout}>Logout</button>
+          {/* Add more profile information here */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
