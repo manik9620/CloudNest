1eba57947f7dbde20310c0877da343b3ca93b085
Manik <manikproch9858@gmail.com>
2024-07-27 12:54:24 +0530

    Added info in PROFILE Page

--- frontend/src/pages/profilePage.js
+++ frontend/src/pages/profilePage.js
@@ -5,6 +5,8 @@ import { appLogout } from "../store/slices/authSlice";
 import Navbar from "../components/navbar/index";
 
 const ProfilePage = () => {
+  const { email, isEmailVerified } = useSelector((e) => e.auth);
+  
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isAuthorized } = useSelector((e) => e.auth);
@@ -26,8 +28,9 @@ const ProfilePage = () => {
         <div className="profile-content">
           <h1>Profile Page</h1>
           <p>Welcome to your profile!</p>
+          <h2>{email}</h2>
+          <p>Verified: {isEmailVerified ? "Yes" : "No"}</p>
           <button onClick={handleLogout}>Logout</button>
-          {/* Add more profile information here */}
         </div>
       </div>
     </>
